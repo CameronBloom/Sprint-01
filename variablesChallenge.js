f0f9d21d64e4ce0ef9f4cda24ef00c4ac4aa54fa
Cameron Morgan <cameron.morgan@skiff.com>
2023-01-25 12:11:40 -0800

    Completed the Variables Challenge.

---
+++ variablesChallenge.js
@@ -0,0 +1,67 @@
+/* ==== Challenge 1: ====
+  - Create a variable named `name`
+  - Assign it's value to your first and last name 
+  - Does this variable need to be a constant? It depends completely on you! It should be a const variable IF you intend to never change your name.
+*/
+
+// code for `name` goes here
+const name = 'Cameron Morgan';
+
+/* ==== Challenge 2: ====
+  - Create a variable called `age`
+  - UNLESS you've descovered a very important secret in life, you should  probably allow for `mutability` here.
+*/
+
+// code for `age` goes here
+let age = 34;
+
+
+/* ==== Challenge 3: ====
+  - Create a variable called `myLocation`
+  - Base the value on where you're currently sitting. Consider if this is something that could change eventually.
+*/
+
+// code for `myLocation` goes here
+let myLocation = 'California';
+
+
+/* ==== Challenge 4: ====
+  - Create a variable called `faveBook` 
+  - Consider if this is some data about yourself that could change.
+*/
+
+// code for `faveBook` goes here
+let faveBook = 'Hyperion'
+
+/* ==== Challenge 5: ====
+  - Create a variable called `faveBand` 
+  - Consider if this is some data about yourself that could change.
+*/
+
+// code for `faveBand` goes here
+let faveBand = 'Alexisonfire'
+
+/* ==== Challenge 6: ====
+  - Problem create a variable called `faveFood` 
+  - Consider if this is some data about yourself that could change.
+*/
+
+// code for `faveFood` goes here
+let faveFood = 'Steak'
+
+/* ==== Challenge 7: ====
+  - Look at the function declaration named `example` below
+  - Comment `block scope` where you see block scope
+  - Comment `function scope` where you see function scope
+*/
+
+function example() {
+  // function scope + block scope
+  if (true) {
+    // block scope
+  }
+}
+
+
+// Check your work against some potential solutions when you're finished: https://codepen.io/lambdaschool/pen/ejbEGN?editors=0012
+
