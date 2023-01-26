e0be837156032afdefe24f207fb51417efe56b56
Cameron Morgan <cameron.morgan@skiff.com>
2023-01-26 12:19:08 -0800

    Completed the Arrays and Array Methods challenges.

---
+++ arraysChallenge.js
@@ -0,0 +1,8 @@
+// Create an array called 'colors' that contains hex codes from https://coolors.co/
+
+// Console.log all the items in your array using bracket notation 
+let colors = ['c9cba3', 'ffe1a8', 'e26d5c', '723d46', '472d30'];
+
+for (let index = 0; index < colors.length; index++) {
+  console.log(colors[index]);
+}
